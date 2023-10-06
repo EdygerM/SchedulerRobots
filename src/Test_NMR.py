eb9254ee36df07c38bec967ae9215b43b72460d0
Edy Mariano <edy.mariano1@gmail.com>
2023-10-06 10:34:31 +0200

    Add socket_client

---
+++ src/Test_NMR.py
@@ -0,0 +1,44 @@
+from universal_robot import UniversalRobot
+import logging
+import time
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+class RobotHandler:
+    def __init__(self):
+        super().__init__()
+        self.urSFC = UniversalRobot("UR_SFC", "172.31.0.14", 7993)
+        self.urNMR = UniversalRobot("UR_NMR", "172.31.0.14", 7992)
+        self.pcNMR = UniversalRobot("PC_NMR", "172.31.0.14", 8001)
+
+        self.urSFC.start_server()
+        self.urNMR.start_server()
+        self.pcNMR.start_server()
+
+
+if __name__ == '__main__':
+    try:
+        logging.info("Starting main loop.")
+        Robots = RobotHandler()
+        input("Input any key when the robots are connected.")
+        print("Program running")
+        running = True
+        while running:
+            Robots.urSFC.send_task("PickStorageNmr")
+            Robots.urSFC.wait_task_end()
+            Robots.urSFC.send_task("PlaceStorageNmr")
+            Robots.urSFC.wait_task_end()
+            Robots.urNMR.send_task("PickStorage")
+            Robots.urNMR.wait_task_end()
+            Robots.urNMR.send_task("PlaceStorage")
+            Robots.urNMR.wait_task_end()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logging.exception("Interrupt received! Stopping servers.")
+    finally:
+
+        time.sleep(1)  # Allow some time for all servers and tasks to stop
+        running = False  # Stop the loop
